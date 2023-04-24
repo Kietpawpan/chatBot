a4946add464f2e792b950aba1ec17fce3d8e7f18
jTech <kietpawpan@gmail.com>
2023-04-24 17:04:51 +0000

    Update iRequest.js

--- js/iRequest.js
+++ js/iRequest.js
@@ -3,7 +3,6 @@ Copyright © 2023 Monte Kietpawpan
 v.1.0.0 | April 24, 2023
 MIT License 
 */
-
 // Get data from Form 1
 function myFunction() {
   var x = document.getElementById("frm1");
@@ -38,51 +37,44 @@ var e = document.getElementById("frm5");
   for (l = 0; l < e.length ;l++) {
     info1 += e.elements[l].value;
   }
-
 var e2 = document.getElementById("frm52");
   var info2 = "";
   var l2;
   for (l2 = 0; l2 < e2.length ;l2++) {
     info2 += e2.elements[l2].value;
   }
-
 var e3 = document.getElementById("frm53");
   var info3 = "";
   var l3;
   for (l3 = 0; l3 < e3.length ;l3++) {
     info3 += e3.elements[l3].value;
   }
-
 var e4 = document.getElementById("frm54");
   var tel = "";
   var l4;
   for (l4 = 0; l4 < e4.length ;l4++) {
     tel += e4.elements[l4].value;
   }
-
-
-
 var opener = "ปลัดกระทรวงทรัพยากรธรรมชาติและสิ่งแวดล้อม";
 var org = "สำนักงานปลัดกระทรวงทรัพยากรธรรมชาติและสิ่งแวดล้อม";
-
    
   // Show the code in the window alert
   window.alert(document.getElementById("iRequest").innerHTML = 
-               "\n\nเรียน " + opener + "\n\nข้าพเจ้า " + text + " หมายเลขบัตรประจำตัวประชาชน " + text2 + " มีความประสงค์ขอข้อมูลข่าวสารของ" + org + 
+            "เรียน " + opener + "\n\nข้าพเจ้า " + text + " หมายเลขบัตรประจำตัวประชาชน " + text2 + " มีความประสงค์ขอข้อมูลข่าวสารของ" + org + 
                         " ดังต่อไปนี้\n" +
             "1. " + info1 + 
             "\n2. " + info2 +
             "\n3. " + info3 +
                         "\nเพื่อนำไปใช้ในการ" + obj +
             "\n\nจึงเรียนมาเพื่อโปรดจัดหาข้อมูลข่าวสารดังกล่าวให้แก่ข้าพเจ้าด้วย จะขอบคุณยิ่ง" +
-            "\n\nขอแสดงความนับถือ" + "\n" + text + "\nโทร. " + tel);
-}
+             "\n\nขอแสดงความนับถือ" + "\n" + text + "\nโทร. " + tel);
+ }
 
-// Copy the text in the textarea filled form and tell the user
-  copyForm() {
-  const element = document.querySelector('#iRequest');
-  element.select();
-  element.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  window.alert("คุณได้คัดลอกร่างคำขอไว้บน clipboard สามารถวางในอีเมลต่อไป");
-}
+ // Copy the text in the textarea filled form and tell the user
+ function copyForm() {
+   const element = document.querySelector('#iRequest');
+   element.select();
+   element.setSelectionRange(0, 99999);
+   document.execCommand('copy');
+   window.alert("คุณได้คัดลอกร่างคำขอไว้บน clipboard สามารถวางในอีเมลต่อไป");
+ }
